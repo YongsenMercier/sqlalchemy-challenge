1405a0d3b613d20eb4819f404070f60d28e902e9
Yongsen Mercier <yongsen.mercier@gmail.com>
2020-09-07 21:39:26 -0400

    The final version

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,50 +1,133 @@
-# 1. import Flask
-from flask import Flask
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# 2. Create an app, being sure to pass __name__
-poo = Flask(__ClimateApp__)
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
 
-# 3. Define what to do when a user hits the index route
-@poo.route("/")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+
+app = Flask(__name__)
+
+
+latestDate = (session.query(Measurement.date)
+                .order_by(Measurement.date.desc())
+                .first())
+latestDate = list(np.ravel(latestDate))[0]
+
+latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
+latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
+latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
+latestDay = int(dt.datetime.strftime(latestDate, '%d'))
+
+yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
+yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
+
+
+
+
+@app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+    return (f"Hawaii Weather API<br/>"
+            f"------------------------------<br/>"
+            f"Please use the following Routes:<br/>"
+            f"/api/v1.0/stations - a list of all weather observation stations<br/>"
+            f"/api/v1.0/precipitaton - the latest year of preceipitation data<br/>"
+            f"/api/v1.0/temperature - the latest year of temperature data<br/>"
+            f"------------------------------<br/>"
+            f"To search please use the following date format: (yyyy-mm-dd)<br/>"
+            f"Date available from 2010-01-01 to 2017-08-23 ~<br/>"
+            f"/api/v1.0/datesearch/2015-05-30  - low, high, and average temp for date given and each date after<br/>"
+            f"/api/v1.0/datesearch/2015-05-30/2016-01-30 - low, high, and average temp for date given and each date up to and including end date<br/>"
+          
+
+            @app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.name).all()
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/precipitaton")
+def precipitation():
     
+    results = (session.query(Measurement.date, Measurement.prcp, Measurement.station)
+                      .filter(Measurement.date > yearBefore)
+                      .order_by(Measurement.date)
+                      .all())
+    
+    precipData = []
+    for result in results:
+        precipDict = {result.date: result.prcp, "Station": result.station}
+        precipData.append(precipDict)
 
+    return jsonify(precipData)
 
-# 4. Define what to do when a user hits the /about route
-@poo.route("/api/v1.0/precipitation")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+@app.route("/api/v1.0/temperature")
+def temperature():
 
+    results = (session.query(Measurement.date, Measurement.tobs, Measurement.station)
+                      .filter(Measurement.date > yearBefore)
+                      .order_by(Measurement.date)
+                      .all())
 
-@poo.route("/api/v1.0/stations")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    tempData = []
+    for result in results:
+        tempDict = {result.date: result.tobs, "Station": result.station}
+        tempData.append(tempDict)
 
-@poo.route("/api/v1.0/tobs")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    return jsonify(tempData)
 
+@app.route('/api/v1.0/datesearch/<startDate>')
+def start(startDate):
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-@poo.route("/api/v1.0/<start>")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    results =  (session.query(*sel)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
+                       .group_by(Measurement.date)
+                       .all())
 
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
 
-@poo.route("/api/v1.0/<start>/<end>")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+@app.route('/api/v1.0/datesearch/<startDate>/<endDate>')
+def startEnd(startDate, endDate):
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
+    results =  (session.query(*sel)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) <= endDate)
+                       .group_by(Measurement.date)
+                       .all())
 
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
 
-
-
-if __ClimateApp__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
